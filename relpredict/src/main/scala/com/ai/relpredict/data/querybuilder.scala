ffc7b04a600ae589e6a27d592daaa60d6a2c666f
mcovert <mcovert@insight.rr.com>
2018-03-26 15:52:33 +0000

    json based conf files. Querybuilder in relpredict

---
+++ relpredict/src/main/scala/com/ai/relpredict/data/querybuilder.scala
@@ -0,0 +1,11 @@
+package com.ai.relpredict.data
+
+import scala.util.parsing._
+import scala.util.parsing.combinator._
+import com.ai.relpredict.dsl._
+import java.io.FileReader
+import java.io.Reader
+import com.ai.relpredict.util._
+import org.apache.log4j.{Level, LogManager, PropertyConfigurator}
+import com.ai.relpredict.spark._
+import org.apache.spark.sql.SparkSession
