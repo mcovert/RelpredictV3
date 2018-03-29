23cb2e8804760a5c4067c239632fc9efb687e898
mcovert <mcovert@insight.rr.com>
2018-03-29 16:26:25 +0000

    Datafile read files from dir code added with parms

--- rpserver/common/models/datafile.js
+++ rpserver/common/models/datafile.js
@@ -18,6 +18,25 @@ module.exports = function(Datafile) {
    			type: 'array'
    		}
    	})
+   Datafile.listAllForBatch = function(batch, cb) {
+   	  var files = fs.readdirSync('/home/mcovert/testfiles/' + batch);
+   	  cb(null, files);
+   } 	
+   Datafile.remoteMethod(
+   	'listAllForBatch', {
+   		http: {
+   			path: '/listAllForBatch',
+   			verb: 'get'
+   		},
+   		accepts: {
+            arg: ' batch',
+            type: 'string'
+   		},
+   		returns: {
+   			arg:  'files',
+   			type: 'array'
+   		}
+   	})
 };
 
 
