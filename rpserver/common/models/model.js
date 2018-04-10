eb822dac56f4e7e2a0b69e7c5837e320f1b51d68
mcovert <mcovert@insight.rr.com>
2018-04-10 15:06:01 +0000

    added nextversion for models

--- rpserver/common/models/model.js
+++ rpserver/common/models/model.js
@@ -2,4 +2,33 @@
 
 module.exports = function(Model) {
 
-};
+    Model.nextversion = function (model_class, model_name, cb) {
+    	console.log('Finding next version for ' + model_class + "/" + model_name);
+    	var version = 1;
+        Model.find({where: {and: [{model_class: model_class}, {name: model_name}]}, order: "version DESC", limit: 1}, function(err, models) {
+        	console.log(models);
+            if (!!models && models.length > 0) version = models[0].version + 1;
+            cb(null, version);
+        });
+    };
+
+    Model.remoteMethod(
+   	'nextversion', {
+   		http: {
+   			path: '/nextversion',
+   			verb: 'get'
+   		},
+   		accepts: [ {
+            arg:  'model_class',
+            type: 'string'
+   		},
+   		{
+   			arg:  'model_name',
+   			type: 'string'
+   		}],
+   		returns: {
+   			arg:  'version',
+   			type: 'number'
+   		}
+   	});
+}
--- rpstudio/src/app/services/model.service.ts
+++ rpstudio/src/app/services/model.service.ts
@@ -11,19 +11,6 @@ import { AuthService } from './auth.service';
 export class ModelService {
 
 httpService    : HttpClient;
-// datatypes      : RPDataType[] = [
-//    { datatype_name : "integer", short_name : "int",     description : "Long integer",
-//      parms: [{ parm_name: "bucket", parm_type: "value", data_type: "integer", description: "Bucket numbers", label: "Number of buckets", parm_default: "0"}]},
-//    { datatype_name : "double",  short_name : "double",  description : "Double precision floating point number",
-//      parms: [{ parm_name: "bucket", parm_type: "value", data_type: "integer", description: "Bucket numbers", label: "Number of buckets", parm_default: "0"}]},
-//    { datatype_name : "boolean", short_name : "boolean", description : "true or false boolean", parms: []},
-//    { datatype_name : "string",  short_name : "string",  description : "Single string value",
-//      parms: [{ parm_name: "case", parm_type: "choose", data_type: "string",  description: "Change case", label: "Translate Case", choose: ["none", "upper", "lower"], parm_default: "none"},
-//              { parm_name: "encode", parm_type: "choose", data_type: "string",description: "Encoding option", label: "Encode as", choose: ["category", "one-hot"], parm_default: "category"}]},
-//    { datatype_name : "text",    short_name : "text",    description : "Multiple delimited string values",
-//      parms: [{ parm_name: "dlm", parm_type: "choose", data_type: "integer",   description: "Bucket numbers", label: "Delimiter", choose: ["tab", "comma", "space", "pipe"], parm_default: "tab"},
-//              { parm_name: "case", parm_type: "choose", data_type: "string",  description: "Change case", label: "Change Case to", choose: ["none", "upper", "lower"], parm_default: "none"}]}
-// ];
 algorithms     : RPAlgorithmDef[] = [
    { name: "Decision Tree", short_name: "dt", description: "Single decision tree", alg_class: "classifier",
      parms: [ {parm_name: "depth", parm_type: "range", data_type: "integer", description: "Maximum tree depth", 
