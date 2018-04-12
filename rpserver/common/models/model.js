18c5fec06b42f771bd4abb59846ff65954636e74
mcovert <mcovert@insight.rr.com>
2018-04-12 00:26:46 +0000

    added setcurrent for models

--- rpserver/common/models/model.js
+++ rpserver/common/models/model.js
@@ -5,13 +5,11 @@ module.exports = function(Model) {
     Model.nextversion = function (model_class, model_name, cb) {
     	console.log('Finding next version for ' + model_class + "/" + model_name);
     	var version = 1;
-        Model.find({where: {and: [{model_class: model_class}, {name: model_name}]}, order: "version DESC", limit: 1}, function(err, models) {
-        	console.log(models);
+      Model.find({where: {and: [{model_class: model_class}, {name: model_name}]}, order: "version DESC", limit: 1}, function(err, models) {
             if (!!models && models.length > 0) version = models[0].version + 1;
             cb(null, version);
-        });
+      });
     };
-
     Model.remoteMethod(
    	'nextversion', {
    		http: {
@@ -29,6 +27,47 @@ module.exports = function(Model) {
    		returns: {
    			arg:  'version',
    			type: 'number'
+   		}
+   	});
+
+    Model.setcurrent = function (model_class, model_name, model_version, cb) {
+    	console.log('Setting current version for ' + model_class + "/" + model_name + " to " + model_version);
+    	var version = 1;
+        Model.findOne({where: {and: [{model_class: model_class}, {name: model_name}, {current: true}]}}, function(err, model) {
+            console.log('old'); console.log(model);
+            if (!!model) model.updateAttribute('current', false, function(err, model) {
+                console.log(model);
+            });
+        });
+        Model.findOne({where: {and: [{model_class: model_class}, {name: model_name}, {version: model_version}]}}, function(err, model) {
+            console.log('new'); console.log(model);
+            if (!!model) model.updateAttribute('current',  true, function(err, model) {
+                console.log(model);
+            });
+        });
+        cb(null, "OK");
+    };
+    Model.remoteMethod(
+   	'setcurrent', {
+   		http: {
+   			path: '/setcurrent',
+   			verb: 'get'
+   		},
+   		accepts: [ {
+            arg:  'model_class',
+            type: 'string'
+   		},
+   		{
+   			arg:  'model_name',
+   			type: 'string'
+   		},
+   		{
+   			arg:  'model_version',
+   			type: 'number'
+   		}],
+   		returns: {
+   			arg:  'success',
+   			type: 'string'
    		}
    	});
 }
