f770e8b8b78be4e1d8d089f1bce0e304edf7b533
mcovert <mcovert@insight.rr.com>
2018-04-25 13:55:22 +0000

    continuing relpredict module

--- rpserver/server/relpredict.js
+++ rpserver/server/relpredict.js
@@ -2,8 +2,10 @@ var fs = require('fs')
 var path = require('path')
 var node_ssh = require('node-ssh')
 var ssh = new node_ssh()
-
-var config = {
+/*******************************************************************************/
+/*    RelPredict system configuration taken from environment variables         */
+/*******************************************************************************/
+exports.config = {
 	home:       process.env.RELPREDICT_HOME,
     uploads:    process.env.RP_UPLOADDIR,
 	archives:   process.env.RP_ARCHIVEDIR,
@@ -13,7 +15,9 @@ var config = {
 	jobs:       process.env.RP_JOBDIR,
 	models:     process.env.RP_MODELDIR
 };
-
+/*******************************************************************************/
+/*                   Job server management functions                           */
+/*******************************************************************************/
 var jobservers = process.env.RP_JOBSERVERS.split(",");
 var running    = new Array(jobservers.length).fill(0);
 var serverNum  = 0;
@@ -33,7 +37,7 @@ getCommandMonitor(cmd, server) {
 	/* Want to use command name to handle this - i.e. spark is port 4040 */
 	return server + ':4040';
 }
-exports.runcmd = (cmd) => {
+exports.runJob = (cmd) => {
     var server = acquireServer();
     ssh.connect({ host: server, username: cmd.username, privateKey: '/home/' + cmd.username + '/.ssh/id_rsa'})
         .then(function() {
@@ -46,4 +50,69 @@ exports.runcmd = (cmd) => {
             releaseServer(server);
         });
     return { 'server': server, 'monitor': getCommandMonitor(cmd.command, server});
+}
+/*******************************************************************************/
+/*                         Data management functions                           */
+/*******************************************************************************/
+var makeFileEntry = function(fullFileName, fileStat) {
+  var fPath = path.parse(fullFileName);
+  var fileName   = fPath.base;
+  var fileFormat = fPath.ext.replace(/\./g,' ').toUpperCase();
+  var dirName    = fPath.dir;
+  var fileType   = '?';
+  if (fullFileName.indexOf('predict')) fileType = 'Predict';
+  else if (fullFileName.indexOf('train')) fileType = 'Train';
+  else if (fullFileName.indexOf('lookup')) fileType = 'Lookup';
+  else if (fullFileName.indexOf('vocab')) fileType = 'Vocabulary';
+  var entry = { 'file_name'  : fileName,
+                'file_type'  : fileType,
+                'file_format': fileFormat,
+                'file_stats' : fileStat};
+  return entry;
+}
+var fileCount = function(dirName) {
+  var num = 0;
+  var files = fs.readdirSync(dirName);
+  for (var i = 0; i < files.length; i++) {
+    var fullFileName = dirName + '/' + files[i];
+    var fileStat = fs.statSync(fullFileName);
+    if (stat.isDirectory()) num += fileCount(fullFileName);
+    else num += 1;
+  }
+  return num;
+}
+exports.getBatches = () => {
+  var entries = [];
+  var files = fs.readdirSync(config.batches);
+  for (var i = 0; i < files.length; i++) {
+    var fullFileName = dirName + '/' + files[i];
+    var fileStat = fs.statSync(fullFileName);
+    if (stat.isDirectory()) {
+    	entries.push( { 'batch_name' : files[i],
+                        'size'       : fileStat.size,
+                        'created'    : fileStat.created,
+                        'files'      : fileCount(fullFileName)
+                      }
+                    );
+    }
+  }
+  return entries;
+}
+exports.getDatafiles = () => {
+
+}
+exports.getBatch = (batch_id) => {
+
+}
+exports.getDatamaps = () => {
+
+}
+exports.getDatamap = (name) => {
+	
+}
+/*******************************************************************************/
+/*                         Model management functions                          */
+/*******************************************************************************/
+exports.convertModel = (model) => {
+
 }
