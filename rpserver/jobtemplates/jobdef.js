cc94949554baf649002489e6f9745decc3492dc7
mcovert <mcovert@insight.rr.com>
2018-04-26 18:16:26 +0000

    job template def completed2

--- rpserver/jobtemplates/jobdef.js
+++ rpserver/jobtemplates/jobdef.js
@@ -3,12 +3,12 @@ var jobtemplates = [
      script: 'train.sh',
      type:   'spark',
      parms: [
-        { parm_name:  'split',      parm_value: '80',    label: 'Split percent'},
-        { parm_name:  'model_def',  parm_value: '',      label: 'Model Definition'},
-        { parm_name:  'data_from',  parm_value: 'sql',   label: 'Get data from'},
-        { parm_name:  'data_def',   parm_value: '',      label: 'SQL, file, or directory'},
-        { parm_name:  'jobname',    parm_value: '',      label: 'Jobname'},
-        { parm_name:  'verbose',    parm_value: 'false', label: 'Verbose output'},
+        { parm_name:  'split',      parm_value: '80',                   label: 'Split percent',   hint: '10 - 90 (80)'},
+        { parm_name:  'model_def',  parm_value: '',                     label: 'Model Definition' hint: 'class, name, and version'},
+        { parm_name:  'data_from',  parm_value: 'sql',                  label: 'Get data from',   hint: 'SQL, File, Directory'},
+        { parm_name:  'data_def',   parm_value: '',                     label: 'SQL, file, or directory', hint: 'SQL, file name, or directory name'},},
+        { parm_name:  'jobname',    parm_value: 'rp_train_${run_date}', label: 'Jobname',         hint: 'Jobname_identifier'},
+        { parm_name:  'verbose',    parm_value: 'false',                label: 'Verbose output',  hint: 'true or false'},
      ]
    },
    { name: 'predict',
