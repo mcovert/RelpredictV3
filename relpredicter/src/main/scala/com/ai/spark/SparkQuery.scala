73eff583088cdbaa9395e74acf0c0e9a4933d3ff
vloewengart <vloewengart@ai26.analyticsinside.us>
2018-05-25 13:43:43 -0400

    Spark code works

--- relpredicter/src/main/scala/com/ai/spark/SparkQuery.scala
+++ relpredicter/src/main/scala/com/ai/spark/SparkQuery.scala
@@ -3,12 +3,21 @@ import org.apache.spark.rdd.RDD
 import org.apache.spark.sql._
 import org.apache.spark.sql.types._
 
-class SparkQuery(val DataSource : String, val SchemaName : String, val TableName : String, val QLimit : String)
+object QueryUtil
 {
+	def SparkQuery( DataSource : String, SchemaName : String, TableName : String, QLimit : String) : Array[(String,String,String)]=
+	{
 
-val sc = SparkSession.builder().appName("Test").config("spark.master", "yarn").enableHiveSupport().getOrCreate() 
-sc.sql("use relpredict")
+		val sc = SparkSession.builder().appName("SparkQuery").config("spark.master", "yarn").enableHiveSupport().getOrCreate() 
+		sc.sql("use "+SchemaName)
 
-val claimsDF =sc.sql("select *  from claim_status where limit 1")
-claimsDF.collect()
+		val queryDF =sc.sql("select *  from "+TableName+" limit "+QLimit)
+		val queryValues=queryDF.map(_.toString).collect.flatMap(_.split(","))
+		val queryTypes=queryDF.dtypes
+		val queryCombined=queryTypes.zip(queryValues)
+		val queryFinal=queryCombined.map((x=>(x._1._1,x._2,x._1._2)))
+
+	return queryFinal
+
+	}
 }
