57d58dd472cde0dafd404af247554231e891c14c
vloewengart <vloewengart@ai26.analyticsinside.us>
2018-06-07 19:46:28 -0400

    Better version of working with multiple columns

--- relpredicter/src/main/scala/com/ai/query/QueryActor.scala
+++ relpredicter/src/main/scala/com/ai/query/QueryActor.scala
@@ -23,9 +23,12 @@ class QueryActor extends Actor with ActorLogging {
      val fullRecord = QueryUtil.SparkQuery(dsource ,dschema,dtable,qlimit)
    //  val tupledRecord  = fullRecord.foreach (r => {(QRecord.apply _).tupled(r) 
    // 												println(r)})
-    val tupledRecord1 = (QRecord.apply _).tupled(fullRecord(0)) 
-    val tupledRecord2 = (QRecord.apply _).tupled(fullRecord(1))
-     sender() ! QRecords(Array(tupledRecord1,tupledRecord2))
+    val tupledRecords : Array[QRecord] = fullRecord.map(r => (QRecord.apply _).tupled(r) )
+    //val tupledRecord1 = (QRecord.apply _).tupled(fullRecord(0)) 
+   // val tupledRecord2 = (QRecord.apply _).tupled(fullRecord(1))
+   // sender() ! QRecords(Array(tupledRecord1,tupledRecord2))
+   sender() ! QRecords(tupledRecords)
+
      	//(QRecord.apply _).tupled(QueryUtil.SparkQuery(dsource ,dschema,dtable,qlimit)(0))
     }
 }
