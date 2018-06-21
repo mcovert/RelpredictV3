b012be5fbc2704ff85a70c6164f7a5cc96bc3b04
vloewengart <vloewengart@ai26.analyticsinside.us>
2018-06-21 12:18:09 -0400

    Modified to reuse context. All works with GET

--- relpredicter/src/main/scala/com/ai/query/QueryActor.scala
+++ relpredicter/src/main/scala/com/ai/query/QueryActor.scala
@@ -10,9 +10,7 @@ final case class QRows(rows:Array[QRow])
 final case class TableName(table_name: String)
 final case class TableList(table_list:Array[TableName])
 
-val sc = SparkSession.builder().appName("SparkQuery").config("spark.master", "local").enableHiveSupport()
-    .config("yarn.resourcemanager.address","ai02.analyticsinside.us:8032")
-    .config("hive.metastore.uris", "thrift://ai04.analyticsinside.us:9083")
+
 
 object QueryActor {
   final case class ActionPerformed(description: String)
@@ -22,18 +20,28 @@ object QueryActor {
   final case class GetTables(source:String, schema:String)
 
   def props: Props = Props[QueryActor]
+
+
+
+   
 }
 
 class QueryActor extends Actor with ActorLogging {
   import QueryActor._
   
+   
+
   override def preStart { 
     println("starting spark session") 
-    
-    sc.getOrCreate() 
+   
   }
+
   override def postStop { 
     println("stopping spark session") 
+    val sc = SparkSession.builder().appName("SparkQuery").config("spark.master", "local").enableHiveSupport()
+    .config("yarn.resourcemanager.address","ai02.analyticsinside.us:8032")
+    .config("hive.metastore.uris", "thrift://ai04.analyticsinside.us:9083")
+    .getOrCreate() 
     sc.stop()
   }
 
@@ -41,13 +49,13 @@ class QueryActor extends Actor with ActorLogging {
  
   def receive: Receive = {
     case GetRecords(source,schema,table,limit) =>
-      val fullRecord = QueryUtil(sc).SparkQuery(source ,schema,table,limit)
+      val fullRecord = QueryUtil.SparkQuery(source ,schema,table,limit)
       // val tupledRecords : Array[QColumn] = fullRecord.map(r => (QColumn.apply _).tupled(r) )
       val tupledRecords : Array[QRow] = fullRecord.map(r=>(QRow.apply (r.map(col => (QColumn.apply _).tupled(col) ))))
       sender() ! QRows(tupledRecords)
 
     case GetTables(source,schema) =>
-      val tablesRecord=QueryUtil(sc).SparkTables(source,schema)
+      val tablesRecord=QueryUtil.SparkTables(source,schema)
       val tablesList : Array[TableName] = tablesRecord.map(r=>(TableName.apply (r.toString)))
       sender() ! TableList(tablesList)
 
