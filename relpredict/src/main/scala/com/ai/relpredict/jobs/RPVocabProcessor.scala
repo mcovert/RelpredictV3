4d89986ab0b9956b4f37a42454745e6ad9e801d8
mcovert <mcovert@insight.rr.com>
2018-06-27 15:43:47 -0400

    refactor model done 4 and ready to test. added rpvocab processor

---
+++ relpredict/src/main/scala/com/ai/relpredict/jobs/RPVocabProcessor.scala
@@ -0,0 +1,42 @@
+package com.ai.relpredict.jobs
+
+import java.io._
+import org.apache.spark.sql._
+
+
+/**
+*  Job to build global vocabularies
+*
+*{{{
+* Command line formats:
+*     rp_vocab config_file
+*}}}        
+*/
+object RPVocabProcessor {
+    private val ss      : SparkSession = SparkSession.builder().appName("rp_vocab").config("spark.master", "yarn").enableHiveSupport().getOrCreate() 
+    private var baseDir : String = "/tmp/"
+
+    def main(args: Array[String]) {
+      import ss.implicits._
+      import ss.sqlContext.implicits._
+      baseDir = if (args(0).endsWith("/")) args(0) else args(0) + "/"
+      val configFile = args(1)
+      val source = scala.io.Source.fromFile(configFile)
+      source.getLines.foreach{l => {
+      	val tokens = l.split(",")
+       	createMapFile(tokens(0), tokens(1))
+      }}
+    }
+    def createMapFile(name: String, sql: String) {
+    	// Run sql Command to get Dataframe and convert to indexed map
+        val df = ss.sqlContext.sql(sql)
+        val map = df.select(name).collect.map(r => r.getString(0)).distinct.zipWithIndex.toMap
+    	// Write Map to HDFS File 
+    	val sb = new StringBuilder()
+    	map.keys.foreach{ k => sb.append(k + "," + map(k) + "\n")}
+        val file = new File(baseDir + name + ".csv")
+        val bw = new BufferedWriter(new FileWriter(file))
+        bw.write(sb.toString)
+        bw.close   		
+    }
+}
